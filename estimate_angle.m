61e8579cd9811872e0216e16741eb6bc283fe0b1
Aline Hubard <aline.hubard@gmail.com>
2015-08-20 15:01:01 -0400

    Fix bug in estimate_angle and make it work for arrays

--- estimate_angle.m
+++ estimate_angle.m
@@ -1,20 +1,21 @@
-function [slope, theta]= estimate_angle(PX,PY)
+function [theta, slope]= estimate_angle(PX,PY)
 
 binsize = 10;
 Ly =400;
+NT = size(PX,2);
+theta = zeros(1,NT); 
+slope = zeros(2,NT);
 
-
-
+for t = 1:NT
+    x = PX(:,t);
+    y = PY(:,t);
     
-    
-    
-    x = PX(PX>0);
-    y = PY(PX>0);
-    [x ix] = sort(ceil(x/binsize));
+    [x, ix] = sort(ceil(x/binsize));
     bindex = [0 ;find(diff(x)>0)];
     y = y(ix);
     ya = zeros(1,length(bindex)-1);
     xa = ya;
+    
     for bin = 1:length(bindex)-1;
         ya(bin) = min(y(bindex(bin)+1:bindex(bin+1)));
         xa(bin) = binsize*bin;
@@ -23,8 +24,9 @@ Ly =400;
     xa = xa(ya>0);
     ya = Ly - ya(ya>0);
     
-    slope = polyfit(xa,ya,1);
-    theta = atan(slope(1))*180/pi;
+    slope(:,t) = polyfit(xa,ya,1);
+    theta(t) = atan(slope(1,t))*180/pi;
+end
     
     
 
