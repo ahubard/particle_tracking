81e375af774b509bcbaec5f0f4ef3af9c3125950
Aline Hubard <aline.hubard@gmail.com>
2015-10-15 11:37:35 -0400

    Fixed bug for empty array in separate_avalanches

--- separate_avalanches.m
+++ separate_avalanches.m
@@ -17,20 +17,25 @@ x_boundary_cuttof = xo-sqrt(R^2-ydiff2_max)+D/2;
 ii_near_boundary = find(PX(:,1)<x_boundary_cuttof);
 r_near_boundary = (PX(ii_near_boundary,1)-xo).^2 + (PY(ii_near_boundary,1)-yo).^2;
 ii_near_boundary = ii_near_boundary(r_near_boundary > (R-.75*D)^2);
-[y_top_layer,  ii_top_layer] = min(PY(ii_near_boundary,1));
-x_top_layer = PX(ii_near_boundary(ii_top_layer),1);
-slope_radial = (y_top_layer-yo)/(x_top_layer-xo);
-slope_boundary = (slope_radial+D/R)/(1-slope_radial*D/R); %tan(D/R)=D/R
-x_boundary = xo-sqrt(R^2/(1+slope_boundary^2));
-y_boundary = slope_boundary*(x_boundary-xo)+yo;
 
-%% Check for particles touching this point in subsequenbt avalanche. 
-ii_near_boundary = find(PY(:) < y_boundary);
-ii_near_boundary = ii_near_boundary(PX(ii_near_boundary) < x_boundary+D);
-go_over_boundary = ((R-D/3)^2- ((PX(ii_near_boundary)-xo).^2 + ...
-    (PY(ii_near_boundary)-yo).^2) < 0);
-Nb_over_boundary = sum(go_over_boundary);
-
+if (isempty(ii_near_boundary))
+    Nb_over_boundary = 0
+else
+    
+    [y_top_layer,  ii_top_layer] = min(PY(ii_near_boundary,1));
+    x_top_layer = PX(ii_near_boundary(ii_top_layer),1);
+    slope_radial = (y_top_layer-yo)/(x_top_layer-xo);
+    slope_boundary = (slope_radial+D/R)/(1-slope_radial*D/R); %tan(D/R)=D/R
+    x_boundary = xo-sqrt(R^2/(1+slope_boundary^2));
+    y_boundary = slope_boundary*(x_boundary-xo)+yo;
+    
+    %% Check for particles touching this point in subsequenbt avalanche.
+    ii_near_boundary = find(PY(:) < y_boundary);
+    ii_near_boundary = ii_near_boundary(PX(ii_near_boundary) < x_boundary+D);
+    go_over_boundary = ((R-D/3)^2- ((PX(ii_near_boundary)-xo).^2 + ...
+        (PY(ii_near_boundary)-yo).^2) < 0);
+    Nb_over_boundary = sum(go_over_boundary);
+end
 
 
     
