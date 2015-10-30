0b83ed09b2a2623135e782a44aab9cc4fae48c8e
Aline Hubard <aline.hubard@gmail.com>
2015-10-30 18:09:15 -0400

    create function to find 2d histogram

--- logplot.m
+++ logplot.m
@@ -5,9 +5,10 @@ variable_number_id = 1;  %Gives which plot to create and their values
 
 figure(1)
 if( variable_number_id == 1)
-    variable = -Size_Potential(ii_whole);
-    exponent = -1.15;
-    normalization_coeff = .21;
+    variable = -Size_Potential(ii_non_spaning);
+    %variable = -Size_Potential(ii_whole);
+    exponent = -1.17;
+    normalization_coeff = .26;
     minnorm = 1;
 elseif ( variable_number_id == 2)
     variable = T(ii_bound_left);
@@ -15,14 +16,15 @@ elseif ( variable_number_id == 2)
     normalization_coeff = .12;
     minnorm = 1/60;
 end
+
 N_bin =31;
  xmin = log10(max(minnorm,min(variable)));
  xmax = log10(max(variable)+10*minnorm);
  x = logspace(xmin,xmax,N_bin);
-%x = 0:.1:14;
+%x = 1:3:124;
 h = histogram(variable,x,'Normalization','pdf');
 y = h.Values;
 figure(2)
-loglog(x(2:end),y,'.-g',x(2:end),normalization_coeff*x(2:end).^exponent);
-%loglog(x(2:end),y,'.-m');
+loglog(x(2:end),y1,'.-',x(2:end),y,'.-',x(2:end),normalization_coeff*x(2:end).^exponent);
+loglog(x(2:end),y,'.-m');
     
