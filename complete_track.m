a084e5bb74e0fbe8ac00bef2103c92533f7f1c06
Aline Hubard <aline.hubard@gmail.com>
2016-02-08 11:02:31 -0500

    Add conditional to function if true its starts the tracking from the begining, if false it starts with the particles positions instead of the images.

--- complete_track.m
+++ complete_track.m
@@ -1,4 +1,4 @@
-function [jj schedulerindex] = complete_track(folder,En)
+function [jj schedulerindex] = complete_track(folder,En,start_from_images)
 
 sprintf('If you wanna track folder %i and experiment series %i press enter, otherwise cancel',folder,En)
 pause()
@@ -7,9 +7,7 @@ sprintf('OK, here we go, we will start by opening the scheduler.')
 sched=findResource('scheduler', 'type', 'jobmanager', 'lookupURL','poincare.engr.ccny.cuny.edu');  %Open scheduler
 
 [git_version, ~] = evalc('system(''git describe --dirty --alway'')')
-%% Check files with images 
-files_index = review_files(folder,En);
-avalanchefiles = zeros(size(files_index));
+
 %% Define your folder and Experiment number En and information file of the experiment;
 
 Cutoff = 11.5;      % minimum peak intensity
@@ -23,56 +21,30 @@ else
     w = 0.7;
 end
 
+
 %% Experiment file
 avanofile = sprintf('/aline%i/rotdrum%i/o%i/Avanonestep%i.mat',folder,folder,En,En);
-%load(avanofile,'avan','mk'); 
-load(avanofile); 
-
+%load(avanofile,'avan','mk');
+load(avanofile);
+    
 schedulerindex = 1;
-
-%% Get background
-Nbfiles = length(files_index);
-bgfile = sprintf('/aline%i/rotdrum%i/o%i/back%i.mat',folder,folder,En,En);
-jumps = ceil(Nbfiles/64);
-
-bgfileexists = whos(matfile(bgfile));
-
-if (length(bgfileexists) < 1)
     
-    for ii = 1:jumps:Nbfiles
-        ni = files_index(ii);
-        jj(schedulerindex) = batch(sched,'getbackground',1,{En,ni,folder,1});
-        schedulerindex = schedulerindex+1;
-    end
+if(start_from_images) %If false, code starts with the position files. 
+    %% Check files with images
+    files_index = review_files(folder,En);
+    avalanchefiles = zeros(size(files_index));
+    %% Get background
+    Nbfiles = length(files_index);
+    bgfile = sprintf('/aline%i/rotdrum%i/o%i/back%i.mat',folder,folder,En,En);
+    jumps = ceil(Nbfiles/64);
     
-    for ii = (-32:-1)+schedulerindex
-        wait(jj(ii));
-    end
+    bgfileexists = whos(matfile(bgfile));
     
-    bk = mk*0;
-    
-    for ii = 1:jumps:Nbfiles
-        ni = files_index(ii);
+    if (length(bgfileexists) < 1)
         
-        if (En > 100)
-            fn=sprintf('/aline%i/rotdrum%i/o%02d/onestep%02d_%05d.mat',folder,folder,En,En,ni);
-        else
-            fn=sprintf('/aline%i/rotdrum%i/o%02d/onestep%02d%05d.mat',folder,folder,En,En,ni);
-        end
-        
-        load(fn,'bk1');
-        bk = max(bk,bk1);
-    end
-    
-    bk1 = bk;
-    save (bgfile,'bk1');
-    
-    sprintf('Done with first part of the background')
-    
-    if(length(bgfileexists) < 2)
         for ii = 1:jumps:Nbfiles
             ni = files_index(ii);
-            jj(schedulerindex) = batch(sched,'getbackground',1,{En,ni,folder,2});
+            jj(schedulerindex) = batch(sched,'getbackground',1,{En,ni,folder,1});
             schedulerindex = schedulerindex+1;
         end
         
@@ -84,44 +56,75 @@ if (length(bgfileexists) < 1)
         
         for ii = 1:jumps:Nbfiles
             ni = files_index(ii);
+            
             if (En > 100)
                 fn=sprintf('/aline%i/rotdrum%i/o%02d/onestep%02d_%05d.mat',folder,folder,En,En,ni);
             else
                 fn=sprintf('/aline%i/rotdrum%i/o%02d/onestep%02d%05d.mat',folder,folder,En,En,ni);
             end
             
-            load(fn,'bk2');
-            bk = max(bk,bk2);
+            load(fn,'bk1');
+            bk = max(bk,bk1);
         end
         
-        bk2 = bk;
-        save (bgfile,'bk2','-append');
-        sprintf('Done with second part of the background')
+        bk1 = bk;
+        save (bgfile,'bk1');
+        
+        sprintf('Done with first part of the background')
+        
+        if(length(bgfileexists) < 2)
+            for ii = 1:jumps:Nbfiles
+                ni = files_index(ii);
+                jj(schedulerindex) = batch(sched,'getbackground',1,{En,ni,folder,2});
+                schedulerindex = schedulerindex+1;
+            end
+            
+            for ii = (-32:-1)+schedulerindex
+                wait(jj(ii));
+            end
+            
+            bk = mk*0;
+            
+            for ii = 1:jumps:Nbfiles
+                ni = files_index(ii);
+                if (En > 100)
+                    fn=sprintf('/aline%i/rotdrum%i/o%02d/onestep%02d_%05d.mat',folder,folder,En,En,ni);
+                else
+                    fn=sprintf('/aline%i/rotdrum%i/o%02d/onestep%02d%05d.mat',folder,folder,En,En,ni);
+                end
+                
+                load(fn,'bk2');
+                bk = max(bk,bk2);
+            end
+            
+            bk2 = bk;
+            save (bgfile,'bk2','-append');
+            sprintf('Done with second part of the background')
+        end
     end
-end
-
-
-%% Find Particle centers using the cluster.
-%schedulerindex = 2*length(1:jumps:Nbfiles)+1;
-
-
-for ii = 1:Nbfiles
-    ni = files_index(ii);
-    jj(schedulerindex) = batch(sched,'findparticlecenters',1,...
-        {En,ni,folder,D,w,Cutoff,MinSep},'Filedependencies',...
-        {'clip.m','chiimg.m','findpeaks.m','ipf.m','discriminate.m'});
-    schedulerindex = schedulerindex+1;
     
+    
+    %% Find Particle centers using the cluster.
+    %schedulerindex = 2*length(1:jumps:Nbfiles)+1;
+    
+    
+    for ii = 1:Nbfiles
+        ni = files_index(ii);
+        jj(schedulerindex) = batch(sched,'findparticlecenters',1,...
+            {En,ni,folder,D,w,Cutoff,MinSep},'Filedependencies',...
+            {'clip.m','chiimg.m','findpeaks.m','ipf.m','discriminate.m'});
+        schedulerindex = schedulerindex+1;
+        
+    end
+    sprintf('Be patient, Im trying to find the centers')
+    
+    %% Wait for cluster to finish finding particle centers.
+    for ii = (-32:-1)+schedulerindex
+        wait(jj(ii));
+    end
+    
+    sprintf ('Done finding centers')
 end
-sprintf('Be patient, Im trying to find the centers') 
-
-%% Wait for cluster to finish finding particle centers.
-for ii = (-32:-1)+schedulerindex
-    wait(jj(ii));
-end
-
-sprintf ('Done finding centers')
-
 % Actualize navfile and separate continuos files in bunches
 for ii = 1:Nbfiles
     ni = files_index(ii);
