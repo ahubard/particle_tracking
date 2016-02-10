6d537c273ef1959eb8e6bbb97528589813100520
Aline Hubard <aline.hubard@gmail.com>
2016-02-10 16:24:44 -0500

    Work in progress to complete the bottom of the drum from previous and posterior pics.

--- compare_rotation.m
+++ compare_rotation.m
@@ -1,3 +1,6 @@
+function  [average_displacement, total_overlap, XI2]  = compare_rotation(alpha_1)
+
+
 folder = 1; 
 En = 103;
 
@@ -9,68 +12,90 @@ rotation_center_file = sprintf('%srotation_center_%i.mat',filedirectory,En);
 load(rotation_center_file);
 
 angle_aperture = 2.35;
-rot_angle = 0.0032;
 i_final = find(diff(Rotation_step(1,:))~=0);
 i_initial = find(diff(Rotation_step(1,:))~=0)+1;d_R = Rotation_step(1,i_final)-Rotation_step(1,i_initial);
-th = d_R*rot_angle;
+nb_rotation_steps_btw_avalanches = Rotation_step(1,i_initial)-Rotation_step(1,i_final);
 
 for ii = 1:length(i_initial)
-    before_fn = sprintf('/aline%i/rotdrum%i/o%02d/positions%02d_%05d.mat',folder,folder,En,En,Fn_imafile(i_final(ii)));
-    after_fn = sprintf('/aline%i/rotdrum%i/o%02d/positions%02d_%05d.mat',folder,folder,En,En,In_imafile(i_initial(ii)));
+    before_fn = sprintf('/aline%i/rotdrum%i/o%02d/positions%02d_%05d.mat',...
+        folder,folder,En,En,Fn_imafile(i_final(ii)));
+    after_fn = sprintf('/aline%i/rotdrum%i/o%02d/positions%02d_%05d.mat',...
+        folder,folder,En,En,In_imafile(i_initial(ii)));
     load(before_fn,'pxs','pys','Npf');
-    pxb(1:Npf(351),ii) = pxs(1:Npf(351),351);
+    pxb(1:Npf(351),ii) = pxs(1:Npf(351),351);%before rotation, after the avalanche finishes.
     pyb(1:Npf(351),ii) = pys(1:Npf(351),351);
     load(after_fn,'pxs','pys','Npf')
-    pxa(1:Npf(1),ii) = pxs(1:Npf(1),1);
+    pxa(1:Npf(1),ii) = pxs(1:Npf(1),1);%after rotation, before the avalanche starts.
     pya(1:Npf(1),ii) = pys(1:Npf(1),1);
 end
 
+
+ 
+
+
 %% Check to see if it matches.
 load(rotation_center_file);
-na = 70;
-d_steps = 30;
-alpha = sum(th(na:na+d_steps))/2;
-tran_pxb = (pxb(:,na)-xo)*cos(alpha)-(pyb(:,na)-yo)*sin(alpha)+xo;
-tran_pyb = (pxb(:,na)-xo)*sin(alpha)+(pyb(:,na)-yo)*cos(alpha)+yo;
-tran_pxa = (pxa(:,na+d_steps)-xo)*cos(-alpha)-(pya(:,na+d_steps)-yo)*sin(-alpha)+xo;
-tran_pya = (pxa(:,na+d_steps)-xo)*sin(-alpha)+(pya(:,na+d_steps)-yo)*cos(-alpha)+yo;
+%% Find rotation angle
+%alpha_1 = .00315; %initial angle. 
+alpha = nb_rotation_steps_btw_avalanches*alpha_1;
+alpha = -repmat(alpha,size(pxb,1),1);
+tran_pxb = (pxb-xo).*cos(alpha)-(pyb-yo).*sin(alpha)+xo;
+tran_pyb = (pxb-xo).*sin(alpha)+(pyb-yo).*cos(alpha)+yo;
 
-plot(tran_pxa,tran_pya,'.',tran_pxb,tran_pyb,'.',x_in,y_in,'k');axis('equal');axis('ij');
 
+X_non_rot = zeros(2210,size(pxa,2));
+Y_non_rot = zeros(2210,size(pxa,2));
+X_rot = zeros(2210,size(pxa,2));
+Y_rot = zeros(2210,size(pxa,2));
+overlaping_number = zeros(1,size(pxa,2));
 %% Find overlap points
+for ii = 1:size(pxa,2)
+[~, trivialbondt1,trivialbondt2] = adjacent(pxa(:,ii),pya(:,ii),tran_pxb(:,ii),tran_pyb(:,ii),2);
 
-[~, trivialbondt1,trivialbondt2] = adjacent(tran_pxa,tran_pya,tran_pxb,tran_pyb,4);
-x1 = tran_pxa((trivialbondt1))';
-x2 = tran_pxb((trivialbondt2))';
-x1x2 = [x1 x2];
-y1 = tran_pya((trivialbondt1))';
-y2 = tran_pyb((trivialbondt2))';
-y1y2 = [y1 y2];
+overlaping_number(ii) = length(trivialbondt1); 
+
+X_non_rot(1:overlaping_number(ii),ii) = pxa(trivialbondt1,ii);
+X_rot(1:overlaping_number(ii),ii) = tran_pxb(trivialbondt2,ii);
+
+Y_non_rot(1:overlaping_number(ii),ii) = pya(trivialbondt1,ii);
+Y_rot(1:overlaping_number(ii),ii) = tran_pyb(trivialbondt2,ii);
+end
 
 
-[low_x il_x] = min(x1x2);
-vx(1) = low_x + D*4;
-vy(1) = y1y2(il_x) +D*4;
+total_diff = sum((X_non_rot-X_rot).^2+(Y_non_rot-Y_rot).^2);
+total_overlap = sum(overlaping_number);
+XI2 = sum(total_diff);
+average_displacement = XI2/total_overlap;
+%y1y2 = [y1 y2];
 
-[low_y il_y] = min(y1y2);
-vy(2) = low_y + D*10;
-vx(2) = x1x2(il_y);
+% 
+% [low_x il_x] = min(x1x2);
+% vx(1) = low_x + D*4;
+% vy(1) = y1y2(il_x) +D*4;
+% 
+% [low_y il_y] = min(y1y2);
+% vy(2) = low_y + D*10;
+% vx(2) = x1x2(il_y);
+% 
+% [max_x im_x] = max(x1x2);
+% vx(3) = max_x - D*4;
+% vy(3) = y1y2(im_x) +D*4;
+% 
+% [max_y im_y] = max(y1y2);
+% vy(4) = max_y;
+% vx(4) = x1x2(im_y);
+% 
+% low_y = -400;
+% max_y = yo+R;
+% ii_b = find(tran_pxb(:,ii) < max_x  &  tran_pxb(:,ii) > low_x  & ...
+%     tran_pyb(:,ii) < max_y & tran_pyb(:,ii) > low_y);
+% ii_a = find(pxa(:,ii) < max_x  &  pxa(:,ii) > low_x  &  pya(:,ii) < max_y &...
+%     pya(:,ii) > low_y);
+%  [~, trivialbondt1,trivialbondt2] = adjacent(pxa(ii_a,ii),pya(ii_a,ii),...
+%      tran_pxb(ii_b,ii),tran_pyb(ii_b,ii),2);
 
-[max_x im_x] = max(x1x2);
-vx(3) = max_x - D*4;
-vy(3) = y1y2(im_x) +D*4;
-
-[max_y im_y] = max(y1y2);
-vy(4) = max_y;
-vx(4) = x1x2(im_y);
-
-low_y = -400;
-max_y = yo+R;
-ii_b = find(tran_pxb<max_x & tran_pxb>low_x & tran_pyb <max_y &  tran_pyb>low_y);
-ii_a = find(tran_pxa <max_x & tran_pxa > low_x & tran_pya < max_y &  tran_pya>low_y);
- [~, trivialbondt1,trivialbondt2] = adjacent(tran_pxa(ii_a),tran_pya(ii_a),tran_pxb(ii_b),tran_pyb(ii_b),3);
- x1 = 
-x_c(:,1) = tran_pxa(ii_a(trivialbondt1))';
-x_c(:,2) = tran_pxb(ii_b(trivialbondt2))';
-y_c(:,1) = tran_pya(ii_a(trivialbondt1))';
-y_c(:,2) = tran_pyb(ii_b(trivialbondt2))';
+% X_non_rot(1:overlaping_number,ii) = pxa(ii_a(trivialbondt1),ii)';
+% Y_non_rot(1:overlaping_number,ii) = pya(ii_a(trivialbondt1),ii)';
+% X_rot(1:overlaping_number,ii)  = tran_pxb(ii_a(trivialbondt2),ii)';
+% Y_rot(1:overlaping_number,ii)  = tran_pyb(ii_a(trivialbondt2),ii)';
+end
--- create_bottom.m
+++ create_bottom.m
@@ -1,9 +1,9 @@
-%% Find the positions of the particles that dont appear in the picture. 
-
+%% Find the positions of the particles that dont appear in the picture.
+tic;
 % Data keeping files
-folder = 1; 
+folder = 1;
 En = 103;
-
+D = 10;
 
 filedirectory = sprintf('/aline%i/rotdrum%i/o%i/',folder,folder,En);
 file_avalanches =sprintf('%sAvalanches_%i.mat',filedirectory,En);
@@ -11,27 +11,89 @@ load(file_avalanches);
 rotation_center_file = sprintf('%srotation_center_%i.mat',filedirectory,En);
 load(rotation_center_file);
 
-%% E 
+%% E
 
 angle_aperture = 2.35;
-rot_angle = 0.0032;
+rot_angle = 0.0031914; %from minimizing distance btw centers in compare_rotation function.
 i_final = find(diff(Rotation_step(1,:))~=0);
-i_initial = find(diff(Rotation_step(1,:))~=0)+1;d_R = Rotation_step(1,i_final)-Rotation_step(1,i_initial);
-th = d_R*rot_angle;
+i_initial = find(diff(Rotation_step(1,:))~=0)+1;
+nb_rotation_steps_btw_avalanches = Rotation_step(1,i_initial)-Rotation_step(1,i_final);
+Dangle_btw_avalanches = nb_rotation_steps_btw_avalanches*rot_angle;
+Tot_angle = cumsum(Dangle_btw_avalanches);
 
+%% Use positions of files after rotation to find the particles before.
+%Number of avalanches after rotating at least the aperture angle
+n_rot = find(Tot_angle > angle_aperture, 1,'first'); 
 
-for ii = 1:length(i_initial)
-    before_fn = sprintf('/aline%i/rotdrum%i/o%02d/positions%02d_%05d.mat',folder,folder,En,En,Fn_imafile(i_final(ii)));
-    after_fn = sprintf('/aline%i/rotdrum%i/o%02d/positions%02d_%05d.mat',folder,folder,En,En,In_imafile(i_initial(ii)));
+%Load initial positions
+before_fn = sprintf('/aline%i/rotdrum%i/o%02d/positions%02d_%05d.mat',...
+        folder,folder,En,En,Fn_imafile(i_final(1)));
+load(before_fn,'pxs','pys','Npf');
+pxo = pxs(1:Npf(351),351);%before rotation, after the avalanche finishes.
+pyo = pys(1:Npf(351),351);
+pxb = zeros(Npf(351),n_rot);
+pyb = zeros(Npf(351),n_rot);
+pxa = zeros(Npf(351),n_rot);
+pya = zeros(Npf(351),n_rot);
+    
+for ii = 1:n_rot
+    before_fn = sprintf('/aline%i/rotdrum%i/o%02d/positions%02d_%05d.mat',...
+        folder,folder,En,En,Fn_imafile(i_final(ii)));
     load(before_fn,'pxs','pys','Npf');
-    pxb(1:Npf(351),ii) = pxs(1:Npf(351),351);
+    pxb(1:Npf(351),ii) = pxs(1:Npf(351),351);%before rotation, after the avalanche finishes.
     pyb(1:Npf(351),ii) = pys(1:Npf(351),351);
+    after_fn = sprintf('/aline%i/rotdrum%i/o%02d/positions%02d_%05d.mat',...
+        folder,folder,En,En,In_imafile(i_initial(ii))); 
     load(after_fn,'pxs','pys','Npf')
-    pxa(1:Npf(1),ii) = pxs(1:Npf(1),1);
+    pxa(1:Npf(1),ii) = pxs(1:Npf(1),1);%after rotation, before the avalanche starts.
     pya(1:Npf(1),ii) = pys(1:Npf(1),1);
 end
 
-%% Check to see if it matches.
+%%
+ii = n_rot;
+[~, trivialbondt1,trivialbondt2] = adjacent(pxa(:,ii-1),pya(:,ii-1),pxb(:,ii),pyb(:,ii),1);
+x = (pxa(trivialbondt1,ii-1) + pxb(trivialbondt2,ii))/2;
+y = (pya(trivialbondt1,ii-1) + pyb(trivialbondt2,ii))/2;
+yh = 400-R*Dangle_btw_avalanches(ii);   %Minimal height of column to fill
+i_zone = find(x > xo & y > (yh-D));
+alpha = Tot_angle(ii+1);
+tran_pxb = (x(i_zone)-xo)*cos(alpha)-(y(i_zone)-yo).*sin(alpha)+xo;
+tran_pyb = (x(i_zone)-xo).*sin(alpha)+(y(i_zone)-yo).*cos(alpha)+yo;
+izone = find(tran_pyb > (400-3*D));
+x_old = tran_pxb(izone);
+y_old = tran_pyb(izone);
+plot(pxo,pyo,'.',x_old,y_old,'.');axis('equal');axis('ij');
+drawnow;
+for ii = n_rot-1:-1:2
 
+%Get positions from the ii rotation
+[~, trivialbondt1,trivialbondt2] = adjacent(pxa(:,ii-1),pya(:,ii-1),pxb(:,ii),pyb(:,ii),1);
+x = (pxa(trivialbondt1,ii-1) + pxb(trivialbondt2,ii))/2;
+y = (pya(trivialbondt1,ii-1) + pyb(trivialbondt2,ii))/2;
+yh = 400-R*Dangle_btw_avalanches(ii);   %Minimal height of column to fill
+i_zone = find(x > xo & y > (yh-2*D)); % Keep only the one unafected by the avalanche.
+alpha = Tot_angle(ii+1);
+%rotate
+tran_pxb = (x(i_zone)-xo)*cos(alpha)-(y(i_zone)-yo).*sin(alpha)+xo;
+tran_pyb = (x(i_zone)-xo).*sin(alpha)+(y(i_zone)-yo).*cos(alpha)+yo;
+plot(tran_pxb,tran_pyb,'.');
+drawnow;
+izone = find(tran_pyb > (400-D)); %Keep only the one outside the original picture.
+tran_pxb = tran_pxb(izone);
+tran_pyb = tran_pyb(izone);
 
-for ii 
+% Find the ones that overlap or come from both rotations
+[~, trivialbondt1,trivialbondt2] = adjacent(x_old,y_old,tran_pxb,tran_pyb,2);
+x_both = tran_pxb(trivialbondt2);
+y_both = tran_pyb(trivialbondt2);
+%Find the particles that are not in both sets. 
+[x_old_e ix] = setdiff(x_old,x_old(trivialbondt1));
+y_old_e = y_old(ix);
+[tran_pxb_e ix] = setdiff(tran_pxb,tran_pxb(trivialbondt2));
+tran_pyb_e = tran_pyb(ix);
+%Positions from all previous rotations
+x_old = [x_both; x_old_e; tran_pxb_e];
+y_old = [y_both; y_old_e; tran_pyb_e];
+end
+
+toc
