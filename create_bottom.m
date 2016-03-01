0154c196c4576d3512768ce0e5cde09b44eacd11
Aline Hubard <aline.hubard@gmail.com>
2016-03-01 18:48:39 -0500

    bugs to be fixed in composition_rotation

--- create_bottom.m
+++ create_bottom.m
@@ -22,26 +22,31 @@ nb_files = length(initialfileindex);
 % The rotation steps t of the files intialfileindex and finalfile index are
 % given by: 
 r_i = avan(2,initialfileindex);
-r_f = avan(2,finalfileindex);
-delta_r = r_i(2:end) - r_f;  %number of rotation steps between avalanches
+% Get files where there were rotation btw them
+[r_i_unique, i_rotate,~] = unique(r_i);
+i_files_to_rotate = initialfileindex(i_rotate);
+f_files_to_rotate = finalfileindex(i_rotate);
 %% Get info about how the center of rotation moves and rotation step size
 center_file = sprintf('%sCenter_%i.mat',filedirectory,En);
 load(center_file);
 a0_x = fit_x.a0;
 a0_y = fit_y.a0;
 th_step = fit_x.w*.7+fit_y.w*.3; %angle per rotation
-steps_to_fill = angle_aperture/th_step;
+steps_to_fill = ceil(angle_aperture/th_step);
 %% Main loop to complete bottom of the circle of all initialfileindexfiles 
 % and get the potential energy before rotation.
 for ii = 1:nb_files
     file_n = initialfileindex(ii);
-    image_fn = sprintf('%s/positions%02d_%05d.mat',filedirectory,file_n);
+    image_fn = sprintf('%s/positions%02d_%05d.mat',filedirectory,En,file_n);
     load(image_fn,'pxs','pys','Npf');
     x_ima = pxs(1:Npf(1),1);
     y_ima = pys(1:Npf(1),1);
-    max_before = r_i(ii) - r_i(1); %Max rotation steps before ii
-    max_after = r_i(end) - r_i(ii); %Max rotationsteps after ii
-    d_r = r_i(ii) - r_i(1:end);
+    max_before = r_i(ii) - r_i(1); % Max rotation steps before ii
+    max_after = r_i(end) - r_i(ii); % Max rotationsteps after ii
+    
+    d_r = r_i(ii) - r_i_unique;
+    ii_new = find(d_r == 0);
+    
     %Find how many rotation steps before and after must be used depending on the
     %current image rotation step.
     if(max_before <= floor(steps_to_fill/2))
@@ -59,68 +64,67 @@ for ii = 1:nb_files
     
     
     % Use previous images
-    if(i_first_ima)
-        for jj = ii-1:-1:i_first_ima
-            file_r = finalfileindex(jj);
-            image_r = sprintf('%s/positions%02d_%05d.mat',filedirectory,file_r);
-            load(image_fn,'pxs','pys','Npf');
-            x = pxs(1:Npf(1),1);
-            y = pys(1:Npf(1),1);
-            %rotate by last rotation angle to check wich positions to keep
-            [~, y_rot] = rotation_composition(x,y,r_i(jj+1),r_i(jj),...
-                A_x,A_y,phi_x,-th_step,a0_x,a0_y);
-            x = x(y_rot >= (max(y) - D));
-            y = y(y_rot >= (max(y) - D));
-            [x_rot, y_rot] = rotation_composition(x,y,r_i(ii),r_i(jj),...
-                A_x,A_y,phi_x,-th_step,a0_x,a0_y);
-            % Find the ones that overlap or come from both files
-            [~, trivialbondt1,trivialbondt2] = adjacent(x_ima,y_ima,x_rot,y_rot,D/2);
-            x_both = (x_rot(trivialbondt2) + x_ima(trivialbondt1))/2;
-            y_both = (y_rot(trivialbondt2) + y_ima(trivialbondt1))/2;
-            %Find the particles that are not in both sets. 
-            [x_ima_e, ix] = setdiff(x_ima,x_ima(trivialbondt1));
-            y_ima_e = y_ima(ix);
-            [x_rot_e, ix] = setdiff(x_rot,x_rot(trivialbondt2));
-            y_rot_e = rot_y(ix);
-            %Positions from all previous rotations
-            x_ima = [x_both; x_ima_e; x_rot_e];
-            y_ima = [y_both; y_ima_e; y_rot_e];
-            %x_ima(length(x_ima)+1:length(x_ima)+length(x)) = x_rot;
-            %y_ima(length(y_ima)+1:length(y_ima)+length(y)) = y_rot;
-        end
+    
+    for jj = ii_new-1:-1:i_first_ima
+        file_r = f_files_to_rotate(jj);
+        image_r = sprintf('%s/positions%02d_%05d.mat',filedirectory,En,file_r);
+        load(image_fn,'pxs','pys','Npf');
+        x = pxs(1:Npf(1),351);
+        y = pys(1:Npf(1),351);
+        %rotate by last rotation angle to check wich positions to keep
+        [x_rot, y_rot] = rotation_composition(x,y,r_i_unique(jj+1),r_i_unique(jj),...
+            A_x,A_y,phi_x,phi_y,-th_step,a0_x,a0_y);
+        x = x(y_rot >= (max(y) - D) & x_rot > (a0_x-A_x));
+        y = y(y_rot >= (max(y) - D));
+        [x_rot, y_rot] = rotation_composition(x,y,r_i(ii),r_i_unique(jj),...
+            A_x,A_y,phi_x,-th_step,a0_x,a0_y);
+        % Find the ones that overlap or come from both files
+        [~, trivialbondt1,trivialbondt2] = adjacent(x_ima,y_ima,x_rot,y_rot,D/2);
+        x_both = (x_rot(trivialbondt2) + x_ima(trivialbondt1))/2;
+        y_both = (y_rot(trivialbondt2) + y_ima(trivialbondt1))/2;
+        %Find the particles that are not in both sets.
+        [x_ima_e, ix] = setdiff(x_ima,x_ima(trivialbondt1));
+        y_ima_e = y_ima(ix);
+        [x_rot_e, ix] = setdiff(x_rot,x_rot(trivialbondt2));
+        y_rot_e = rot_y(ix);
+        %Positions from all previous rotations
+        x_ima = [x_both; x_ima_e; x_rot_e];
+        y_ima = [y_both; y_ima_e; y_rot_e];
+        %x_ima(length(x_ima)+1:length(x_ima)+length(x)) = x_rot;
+        %y_ima(length(y_ima)+1:length(y_ima)+length(y)) = y_rot;
     end
     
-    %Use posterior images
-    if(i_last_ima)
-        for jj = ii+1:i_last_ima
-            file_r = initialfileindex(jj);
-            image_r = sprintf('%s/positions%02d_%05d.mat',filedirectory,file_r);
-            load(image_fn,'pxs','pys','Npf');
-            x = pxs(1:Npf(1),1);
-            y = pys(1:Npf(1),1);
-            %rotate by last rotation angle to check wich positions to keep
-            [~, y_rot] = rotation_composition(x,y,r_i(jj-1),r_i(jj),...
-                A_x,A_y,phi_x,-th_step,a0_x,a0_y);
-            x = x(y_rot >= (max(y)-10));
-            y = y(y_rot >= (max(y)-10));
-            [x_rot, y_rot] = rotation_composition(x,y,r_i(ii),r_i(jj),...
-                A_x,A_y,phi_x,-th_step,a0_x,a0_y);
-            % Find the ones that overlap or come from both files
-            [~, trivialbondt1,trivialbondt2] = adjacent(x_ima,y_ima,x_rot,y_rot,D/2);
-            x_both = (x_rot(trivialbondt2) + x_ima(trivialbondt1))/2;
-            y_both = (y_rot(trivialbondt2) + y_ima(trivialbondt1))/2;
-            %Find the particles that are not in both sets. 
-            [x_ima_e, ix] = setdiff(x_ima,x_ima(trivialbondt1));
-            y_ima_e = y_ima(ix);
-            [x_rot_e, ix] = setdiff(x_rot,x_rot(trivialbondt2));
-            y_rot_e = rot_y(ix);
-            %Positions from all previous rotations
-            x_ima = [x_both; x_ima_e; x_rot_e];
-            y_ima = [y_both; y_ima_e; y_rot_e];
-            %x_ima(length(x_ima)+1:length(x_ima)+length(x)) = x_rot;
-            %y_ima(length(y_ima)+1:length(y_ima)+length(y)) = y_rot;
-        end
+    
+    %Use posterior image
+    for jj = ii_new+1:i_last_ima
+        file_r = i_files_to_rotate(jj);
+        image_r = sprintf('%s/positions%02d_%05d.mat',filedirectory,file_r);
+        load(image_fn,'pxs','pys','Npf');
+        x = pxs(1:Npf(1),1);
+        y = pys(1:Npf(1),1);
+        %rotate by last rotation angle to check wich positions to keep
+        [~, y_rot] = rotation_composition(x,y,r_i_unique(jj-1),r_i_unique(jj),...
+            A_x,A_y,phi_x,-th_step,a0_x,a0_y);
+        x = x(y_rot >= (max(y)-D));
+        y = y(y_rot >= (max(y)-D));
+        [x_rot, y_rot] = rotation_composition(x,y,r_i(ii),r_i_unique(jj),...
+            A_x,A_y,phi_x,-th_step,a0_x,a0_y);
+        % Find the ones that overlap or come from both files
+        [~, trivialbondt1,trivialbondt2] = adjacent(x_ima,y_ima,x_rot,y_rot,D/2);
+        x_both = (x_rot(trivialbondt2) + x_ima(trivialbondt1))/2;
+        y_both = (y_rot(trivialbondt2) + y_ima(trivialbondt1))/2;
+        %Find the particles that are not in both sets.
+        [x_ima_e, ix] = setdiff(x_ima,x_ima(trivialbondt1));
+        y_ima_e = y_ima(ix);
+        [x_rot_e, ix] = setdiff(x_rot,x_rot(trivialbondt2));
+        y_rot_e = rot_y(ix);
+        %Positions from all previous rotations
+        x_ima = [x_both; x_ima_e; x_rot_e];
+        y_ima = [y_both; y_ima_e; y_rot_e];
+        %x_ima(length(x_ima)+1:length(x_ima)+length(x)) = x_rot;
+        %y_ima(length(y_ima)+1:length(y_ima)+length(y)) = y_rot;
     end
+   
     
     file_save = sprintf('%sComplete_positions_%i.mat',filedirectory,file_n);
     save(file_save,'x_ima','y_ima');
--- rotation_composition.m
+++ rotation_composition.m
@@ -1,4 +1,4 @@
-function [x_rot, y_rot] = rotation_composition(x,y,o_t,r_t,A_x,A_y,phi_x,frequency,a0_x,a0_y)
+function [x_rot, y_rot] = rotation_composition(x,y,o_t,r_t,A_x,A_y,phi_x,phi_y, frequency,a0_x,a0_y)
 % rotation_composition rotates coordinates x,y from rotation_image to the
 % reference of the original image. Adds a correction over the fact that the
 % rotation center of the image moves as the drum rotates.
@@ -11,14 +11,14 @@ n_rot = r_t - o_t;
 t = o_t:sign(n_rot)*1:r_t;  %List of rotation steps between o_t and r_t
 %Positions of the center btw o_t and r_t
 xo = A_x*sin(frequency*t+phi_x)+a0_x;   
-yo = A_y*cos(frequency*t+phi_x-.2)+a0_y;
+yo = A_y*cos(frequency*t+phi_y)+a0_y;
 %Get difference btw xo and yo to apply the rotation matrices
 dxo = diff(xo);
 dyo = diff(yo);
 theta = (t(end-1:-1:1)-o_t)*frequency; %Angle that will be rotated
 [rot_correction_x, rot_correction_y] = rot_me(theta,-dxo,-dyo);
-rot_correction_x = sum(rot_correction_x) + xo(end);
-rot_correction_y = sum(rot_correction_y) + yo(end);
+rot_correction_x = sum(rot_correction_x);
+rot_correction_y = sum(rot_correction_y);
 [x_rot, y_rot]= rot_me(n_rot*frequency,x,y);
 x_rot = x_rot + rot_correction_x;
 y_rot = y_rot + rot_correction_y;
