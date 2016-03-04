d4a0ada4ffff94f4222da9f9c7bc50382e638810
Aline Hubard <aline.hubard@gmail.com>
2016-03-04 17:32:24 -0500

    Find the phase on each experiment

--- modify_center_func.m
+++ modify_center_func.m
@@ -1,37 +1,62 @@
+function phi_x = modify_center_function(folder,En)
 %% Get rid of outliers and use that the frequency is already given for the fit. 
-
 [git_version, ~] = evalc('system(''git describe --dirty --alway'')');
 
 
 %% Use images and angle of rotation to find how the center of rotation moves
-%filedirectory = sprintf('/aline%i/rotdrum%i/o%i/',folder,folder,En);
-%file_for_center = sprintf('%sCenter_%i.mat',filedirectory,En);
-%file_for_center = '/Users/Aline/Documents/Research/MATLAB/Avalanches/
-%file_for_center
-%load(file_for_center,'git_version','nb_rotation_steps','xot','yot','Rt');
+filedirectory = sprintf('/aline%i/rotdrum%i/o%i/',folder,folder,En);
+file_for_center = sprintf('%sCenter_%i.mat',filedirectory,En);
+
+
+load(file_for_center,'git_version','nb_rotation_steps','xot','yot','Rt');
 w = 0.0032;
 %th = Total_rotation_steps*w;
 th = nb_rotation_steps*w;
 %% First get rid of outliers by doing a linear aproximattion
 a0_x = mean(xot);
 include_x = find(abs(xot-a0_x) < 6);
-a0_y = mean(yot);
-include_y = find(abs(yot-a0_y) < 6);
+% a0_y = mean(yot);
+% include_y = find(abs(yot-a0_y) < 6);
 
 
-ft = fittype({'sin(x)','cos(x)','1'},'coefficients',{'b','c','a0'});
-ft_x = fit(th(include_x)',xot(include_x)',ft);
-% get xot = Asin(th+phi_x)+a0
-phi_x = atan(ft_x.c/ft_x.b);
-a0_x = ft_x.a0;
-A_x = ft_x.b/cos(phi_x);
+% ft = fittype({'sin(x)','cos(x)','1'},'coefficients',{'b','c','a0'});
+% ft_x = fit(th(include_x)',xot(include_x)',ft);
+% % get xot = Asin(th+phi_x)+a0
+% phi_x = atan(ft_x.c/ft_x.b);
+% a0_x = ft_x.a0;
+% A_x = ft_x.b/cos(phi_x);
 
 %% Used first to get the relation btw xot and yot
-ft_y = fit(th(include_y)',yot(include_y)',ft);
-%get xot = Asin(th+phi_x)+a0
-phi_y = atan(-ft_y.b/ft_y.c);
-a0_y = ft_y.a0;
-A_y = ft_y.c/cos(phi_y);
-%% Use info from previuos fit for files En>100
-Amplitud_coef = A_y/A_x;
-delta_phi = phi_y - phi_x;
+% ft_y = fit(th(include_y)',yot(include_y)',ft);
+% %get xot = Asin(th+phi_x)+a0
+% phi_y = atan(-ft_y.b/ft_y.c);
+% a0_y = ft_y.a0;
+% A_y = ft_y.c/cos(phi_y);
+% Amplitud_coef = A_y/A_x;
+% delta_phi = phi_y - phi_x;
+%% Use that all files have Ay = Ax and phi_y = phi_x + .2;
+
+%% Use info from previuos fit for files En < 100
+if(En <100)
+    Amp = 1.16;
+    a0_x = 633.6;
+    a0_y = 203;
+else
+    Amp = 1.7;
+    a0_x = 643.45;
+    a0_y = 159.6;
+end
+    
+%% Rescale data for new fit and find the phase change
+n_xot = (xot - a0_x)/Amp;
+ft = fittype({'sin(x)','cos(x)'},'coefficients',{'b','c'});
+n_fit = fit(th(include_x)',n_xot(include_x)',ft);
+b = n_fit.b; %for correction of phase if amplitude is positive
+phi_x = atan(n_fit.c/n_fit.b) + pi*(sign(b)-1)/2;
+phi_x = mod(phi_x,2*pi);
+% n_yot = (yot - a0_y)/Amp;
+% n_th = th + phi_x;
+% n_fit = fit(th(include_y)',n_yot(include_y)',ft);
+% b_y = n_fit.b;
+% d_phi = atan(-n_fit.b/n_fit.c);
+
