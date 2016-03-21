70634b0415c3566d592f2923443b7459bde89e26
ahubard <aline.hubard@gmail.com>
2016-03-21 15:56:23 -0400

    Update cutoff values

--- create_bottom.m
+++ create_bottom.m
@@ -14,15 +14,15 @@ load(avanofile);
 % navfile has the numbers of files that contain avalanches. avan(1,:)
 % indicates if the files are continuos in time or broke. If
 % avan(1,n)=avan(1,n+1) then files n and n+1 belong to a continuos set of time.
-changefileindex = find(diff(avan(1,navfile))>1);  
+changefileindex = find(diff(avan(1,navfile))>1);
 initialfileindex = navfile([1 changefileindex(1:end-1)+1]);%first file of set
-finalfileindex = navfile(changefileindex);%last file of set. 
+finalfileindex = navfile(changefileindex);%last file of set.
 % Task is to recreate the bottom of the first image of files
 % initialfileindex.
 nb_files = length(initialfileindex);
 % Between finalfileindex(n) and initialfileindex(n+1) a rotation ocurred.
 % The rotation steps t of the files intialfileindex and finalfile index are
-% given by: 
+% given by:
 r_i = avan(2,initialfileindex);
 % Get files where there were rotation btw them
 [r_i_unique, i_rotate,~] = unique(r_i);
@@ -33,39 +33,39 @@ N_particles = zeros(1,nb_files);
 x_cm = zeros(1,nb_files);
 y_cm = zeros(1,nb_files);
 
- N_rot_particles = zeros(1,nb_files);
- x_from_rot_cm = zeros(1,nb_files);
- y_from_rot_cm = zeros(1,nb_files);
+N_rot_particles = zeros(1,nb_files);
+x_from_rot_cm = zeros(1,nb_files);
+y_from_rot_cm = zeros(1,nb_files);
 %% Get info about how the center of rotation moves and rotation step
 [A, a0_x, a0_y, phi_x] = modify_center_func(folder,En);
 phi_y = phi_x + 0.1;
 % a0_x = fit_x.a0;
 % a0_y = fit_y.a0;
- th_step = 0.0032; %angle per rotation
+th_step = 0.0032; %angle per rotation
 steps_to_fill = ceil(angle_aperture/th_step);
-%% Main loop to complete bottom of the circle of all initialfileindexfiles 
+%% Main loop to complete bottom of the circle of all initialfileindexfiles
 % and get the potential energy before rotation.
 for ii = 1:nb_files
-
+    
     %Extras per image
     x_from_rot = [];
     y_from_rot = [];
-
+    
     
     file_n = initialfileindex(ii);
     Track_file = sprintf('%sDisplacement_%i.mat',filedirectory, ii);
     load(Track_file,'initial','final','diskmove','PX','PY');
-
+    
     if(file_n ~= initial)
         save(sprintf('%sError_%i.mat',filedirectory,ii),'file_n','initial');
         error('Somethings not right');
     end
-
+    
     image_fn = sprintf('%s/positions%02d_%05d.mat',filedirectory,En,file_n);
     load(image_fn,'pxs','pys','Npf');
     x_ima = pxs(1:Npf(1),1);
     y_ima = pys(1:Npf(1),1);
-
+    
     max_before = r_i(ii) - r_i(1); % Max rotation steps before ii
     max_after = r_i(end) - r_i(ii); % Max rotationsteps after ii
     
@@ -96,14 +96,14 @@ for ii = 1:nb_files
         file_r = f_files_to_rotate(jj);
         Track_file = sprintf('%sDisplacement_%i.mat',filedirectory, i_rotate(jj));
         load(Track_file,'initial','final','diskmove','PX','PY');
-
-    if(file_r ~= final)
-        save(sprintf('%sError_%i.mat',filedirectory,ii),'file_n','initial','final','jj');
-        error('Somethings not right');
-    end
+        
+        if(file_r ~= final)
+            save(sprintf('%sError_%i.mat',filedirectory,ii),'file_n','initial','final','jj');
+            error('Somethings not right');
+        end
         PY = 400 - PY;
         position_change = sqrt((PX(:,end)-PX(:,1)).^2+(PY(:,end)-PY(:,1)).^2);
-        particles_move = find(position_change >= 4.5);
+        particles_move = find(position_change >= 4);
         
         image_r = sprintf('%s/positions%02d_%05d.mat',filedirectory,En,file_r);
         load(image_r,'pxs','pys','Npf');
@@ -111,7 +111,7 @@ for ii = 1:nb_files
         x = pxs(1:Npf(t),t);
         y = pys(1:Npf(t),t);
         % Keep particles that didnt move much in previous frames
-        adjacentmatrix = adjacent(x, y, PX(particles_move,end), PY(particles_move,end), D/2);
+        adjacentmatrix = adjacent(x, y, PX(particles_move,end), PY(particles_move,end), D/2+1);
         particles_move = find(sum(adjacentmatrix,2));
         p_to_rotate = setdiff(1:length(x),particles_move);
         x_aux = x(p_to_rotate);
@@ -125,8 +125,8 @@ for ii = 1:nb_files
             A,A,phi_x,phi_y,th_step,a0_x,a0_y);
         x_rot = x_rot(y_rot >= (max(y)-3*D));
         y_rot = y_rot(y_rot >= (max(y)-3*D));
-         plot(x_ima,y_ima,'.',x_rot,y_rot,'.');axis('equal');
-         drawnow;
+        plot(x_ima,y_ima,'.',x_rot,y_rot,'.');axis('equal');
+        drawnow;
         [x_ima, y_ima, x_non_ima, y_non_ima, o_s] = merge_positions(x_ima,y_ima,x_rot,y_rot);
         if (o_s(1) == 0 || o_s(2) == 0)
             save(sprintf('%sCheck_bottom_%i.mat',filedirectory, file_n),'file_r')
@@ -144,14 +144,14 @@ for ii = 1:nb_files
         file_r = i_files_to_rotate(jj);
         Track_file = sprintf('%sDisplacement_%i.mat',filedirectory, i_rotate(jj));
         load(Track_file,'initial','final','diskmove','PX','PY');
-
-    if(file_r ~= initial)
-        save(sprintf('%sError_%i.mat',filedirectory,ii),'file_n','initial','final','jj');
-        error('Somethings not right');
-    end
+        
+        if(file_r ~= initial)
+            save(sprintf('%sError_%i.mat',filedirectory,ii),'file_n','initial','final','jj');
+            error('Somethings not right');
+        end
         PY = 400 - PY;
         position_change = sqrt((PX(:,end)-PX(:,1)).^2+(PY(:,end)-PY(:,1)).^2);
-        particles_move = find(position_change >= 4.5);
+        particles_move = find(position_change >= 4);
         
         image_r = sprintf('%s/positions%02d_%05d.mat',filedirectory,En,file_r);
         load(image_r,'pxs','pys','Npf');
@@ -160,7 +160,7 @@ for ii = 1:nb_files
         y = pys(1:Npf(t),t);
         
         % Keep particles that didnt move much in previous frames
-        adjacentmatrix = adjacent(x, y, PX(particles_move,end), PY(particles_move,end), D/2);
+        adjacentmatrix = adjacent(x, y, PX(particles_move,end), PY(particles_move,end), D/2 + 1);
         particles_move = find(sum(adjacentmatrix,2));
         p_to_rotate = setdiff(1:length(x),particles_move);
         x_aux = x(p_to_rotate);
@@ -168,7 +168,7 @@ for ii = 1:nb_files
         x = x_aux(x_aux > (a0_x+D)  & y_aux > max(y)-1.5*D-m*(x_aux-a0_x));
         y = y_aux(x_aux > (a0_x+D)  & y_aux > max(y)-1.5*D-m*(x_aux-a0_x));
         
-         %rotate positions to meet ima positions        
+        %rotate positions to meet ima positions
         [x_aux, y_aux] = rotation_composition(x,y,r_i(ii),r_i_unique(jj),...
             A,A,phi_x,phi_y,th_step,a0_x,a0_y);
         x_rot = x_aux(x_aux >= 1/mtheta*(y_aux-yo)+xo-5*D  & y_aux >(max(y)-3*D));
@@ -183,22 +183,22 @@ for ii = 1:nb_files
         y_from_rot = [y_from_rot(:); y_non_ima(:)];
     end
     toc
-   N_particles(ii) = length(x_ima);
-   x_cm(ii) = mean(x_ima);
-   y_cm(ii) = mean(y_ima);
-   
-   N_rot_particles(ii) = length(x_from_rot);
-   x_from_rot_cm(ii) = mean(x_from_rot);
-   y_from_rot_cm(ii) = mean(y_from_rot);
+    N_particles(ii) = length(x_ima);
+    x_cm(ii) = mean(x_ima);
+    y_cm(ii) = mean(y_ima);
+    
+    N_rot_particles(ii) = length(x_from_rot);
+    x_from_rot_cm(ii) = mean(x_from_rot);
+    y_from_rot_cm(ii) = mean(y_from_rot);
     
     file_save_i = sprintf('%sComplete_positions_%i.mat',filedirectory,file_n);
     save(file_save_i,'x_ima','y_ima','x_from_rot','y_from_rot');
     
 end
-[git_version, ~] = evalc('system(''git describe --dirty --alway'')');  
+[git_version, ~] = evalc('system(''git describe --dirty --alway'')');
 file_save_CM = sprintf('%sCenter_of_Mass_%i.mat',filedirectory, En);
 save(file_save_CM,'N_particles','x_cm','y_cm','N_rot_particles',...
     'x_from_rot_cm','y_from_rot_cm','git_version');
-            
-            
-   %toc
+
+
+%toc
