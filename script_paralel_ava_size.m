1f0d2f46898f2c3245fe22a48002ae7c37fc8248
Aline Hubard <aline.hubard@gmail.com>
2016-07-12 16:56:29 -0400

    Add script to run avalanche_size for different experiments

---
+++ script_paralel_ava_size.m
@@ -0,0 +1,18 @@
+%% Calls the function avalanche_size, for the different experiments in all 3 kinds. 
+%uses a parallel loop to use the cluster and try to reduce the time. 
+
+%% Create the array of different initial positions.
+Experiment = [ 15 16 17 18 19 20 21 22 23 103 104 105 106 107 108 109 ];
+FOLDER = [1 1 1 1 1 1 2 2 2 1 1 2 2 2 2 2];
+LE = length(Experiment);
+%% Open matlab in parlel mode
+matlabpool open
+%regular loop over kind of avalanche separation
+for kind = 0:2 
+    parfor ie = 1:LE
+        avalanche_size(FOLDER(ie),Experiment(ie),kind);
+    end
+    sprintf('Done with kind = %i',kind);
+end
+
+    
